9eca0768686f41dee0d58c0abd5abdb0249a3951
QahtanAlJammali <qahtan@nyu.edu>
2024-09-30 14:25:37 -0400

    adding settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-84o6f4s5wiardy257xm73x^q_i@!v#qb^!s22dc$_=#6mauuyj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysite-env.eba-yppkmvr4.us-west-2.elasticbeanstalk.com",
+                 "127.0.0.1"]
 
 
 # Application definition
