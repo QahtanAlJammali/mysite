bbb08119f6a297a8ec4240c3ac23a1d566f02365
QahtanAlJammali <qahtan@nyu.edu>
2024-10-13 17:26:25 -0400

    added tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
+class SampleTest(TestCase):
+    def test_example(self):
+        self.assertTrue(True)
 # Create your tests here.
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,4 +1,2 @@
-from django.test import TestCase  # noqa: F401
-
 def test_addition():
     assert 1 + 1 == 2
