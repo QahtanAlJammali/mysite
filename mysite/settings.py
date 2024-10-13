ef57c9b2afc789957b02a3682bc1f58fc3ce428a
QahtanAlJammali <qahtan@nyu.edu>
2024-10-13 15:26:30 -0400

    reformatting using black

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,15 @@ SECRET_KEY = "django-insecure-84o6f4s5wiardy257xm73x^q_i@!v#qb^!s22dc$_=#6mauuyj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mysitev2-env.eba-2w9f9be5.us-west-2.elasticbeanstalk.com",'172.31.31.23']
+ALLOWED_HOSTS = [
+    "mysitev2-env.eba-2w9f9be5.us-west-2.elasticbeanstalk.com",
+    "172.31.31.23",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
